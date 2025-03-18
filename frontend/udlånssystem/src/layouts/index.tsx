6224f2a63efe22f999b1eb4b89843b92a961b8e5
Nima Badiei <nimabadiei@gmail.com>
2025-03-18 15:31:13 +0100

    Add showNew prop to Layout component for conditional rendering of 'New' button

--- frontend/udlånssystem/src/layouts/index.tsx
+++ frontend/udlånssystem/src/layouts/index.tsx
@@ -10,9 +10,14 @@ import { columnsFormatter } from "@helpers/tableHelpers";
 interface LayoutProps {
   table: string | DataWithHeaders<unknown> | null;
   exclude?: string[];
+  showNew?: boolean;
 }
 
-export default function Layout({ table, exclude }: LayoutProps) {
+export default function Layout({
+  table,
+  exclude,
+  showNew = true,
+}: LayoutProps) {
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -46,14 +51,16 @@ export default function Layout({ table, exclude }: LayoutProps) {
 
   return (
     <div className="h-full w-full overflow-hidden p-4">
-      <Button asChild>
-        <Link
-          to={`${location.pathname}/new`}
-          className="absolute right-3 top-3 h-9"
-        >
-          New
-        </Link>
-      </Button>
+      {showNew && (
+        <Button asChild>
+          <Link
+            to={`${location.pathname}/new`}
+            className="absolute right-3 top-3 h-9"
+          >
+            New
+          </Link>
+        </Button>
+      )}
       <Table
         columns={columns}
         data={inputData.data}
