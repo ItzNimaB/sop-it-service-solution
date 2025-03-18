996e772a01e84567a86ee891e7498006dad462e5
Nima Badiei <nimabadiei@gmail.com>
2025-03-18 10:11:21 +0100

    Add Accordion component for active & returned loan products display

--- frontend/udlånssystem/src/pages/udlaan/edit/components/loanProducts.tsx
+++ frontend/udlånssystem/src/pages/udlaan/edit/components/loanProducts.tsx
@@ -1,6 +1,15 @@
 import { Link, useParams } from "react-router-dom";
 
+import { Button } from "@components/ui/button";
+
 import useData from "@hooks/useData";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function LoanProducts() {
   const { id } = useParams() as any;
@@ -11,22 +20,53 @@ export default function LoanProducts() {
 
   if (!itemsFromLoans?.length) return null;
 
+  const activeItems = itemsFromLoans.filter((item) => !item.Returneret);
+  const returnedItems = itemsFromLoans.filter((item) => item.Returneret);
+
   return (
-    <div className="flex flex-col gap-1">
-      <h1>Produkter:</h1>
-      <hr />
-      <div className="flex flex-col">
-        {itemsFromLoans.map((item, i) => (
-          <Link
-            key={i}
-            className="flex items-center gap-3"
-            to={"/produkter/" + item.UUID}
-          >
-            <span>{item.Produkt}</span>
-            <i className="fa-solid fa-up-right-from-square cursor-pointer" />
-          </Link>
-        ))}
-      </div>
-    </div>
+    <Accordion
+      type="multiple"
+      className="flex flex-col gap-5"
+      defaultValue={["activeItems"]}
+    >
+      <AccordionItem value="activeItems" className="flex flex-col gap-1">
+        <AccordionTrigger className="!border-0">
+          Lånte produkter:
+        </AccordionTrigger>
+        <AccordionContent className="flex flex-col">
+          {activeItems.map((item, i) => (
+            <ItemLink item={item} key={i} />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="returnedItems" className="flex flex-col gap-1">
+        <AccordionTrigger className="!border-0">
+          Returnerede produkter:
+        </AccordionTrigger>
+
+        <AccordionContent className="flex flex-col">
+          {returnedItems.map((item, i) => (
+            <ItemLink item={item} key={i} />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+function ItemLink({ item }: { item: itemsFromLoan }) {
+  return (
+    <Button
+      asChild
+      variant="link"
+      className="h-auto items-start justify-start py-1"
+      key={item.UUID}
+    >
+      <Link className="flex items-center gap-3" to={"/produkter/" + item.UUID}>
+        <span>{item.Produkt}</span>
+        {/* <i className="fa-solid fa-up-right-from-square cursor-pointer" /> */}
+      </Link>
+    </Button>
   );
 }
