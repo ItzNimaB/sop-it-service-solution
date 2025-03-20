798dd8b0628c2997f9a7619f3936720346802699
Nima Badiei <nimabadiei@gmail.com>
2025-03-20 18:41:53 +0100

    Set default base URL for axios to fallback to "/api" if not defined

--- frontend/udlånssystem/src/axios.config.ts
+++ frontend/udlånssystem/src/axios.config.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL;
+axios.defaults.baseURL = import.meta.env.VITE_APP_BACKEND_URL || "/api";
 axios.defaults.withCredentials = true;
