c95c16da46604931617da47c9b992ffcd035f817
Nima Badiei <nimabadiei@gmail.com>
2025-04-11 10:41:25 +0200

    feat: Enhance getAll function to support filtering user loans by moderator level, username, and user_id

--- backend/src/api/controllers/user_loans.ts
+++ backend/src/api/controllers/user_loans.ts
@@ -2,7 +2,14 @@ import * as UserLoansService from "@services/user_loans";
 
 export function GetAll(): IController {
   return async (req, res) => {
-    const response = await UserLoansService.getAll();
+    const { moderatorLevel, username } = req.user || {};
+    let user_id = req.query.user_id;
+
+    const response = await UserLoansService.getAll(
+      moderatorLevel,
+      username,
+      user_id ? Number(user_id) : undefined
+    );
 
     res.status(response.status).json(response.data);
   };
--- backend/src/api/services/user_loans.ts
+++ backend/src/api/services/user_loans.ts
@@ -1,7 +1,22 @@
 import prisma from "@/configs/prisma.config";
 
-export async function getAll(): Promise<IResponse> {
-  const userLoans = await prisma.user_loans.findMany();
+export async function getAll(
+  moderatorLevel?: number,
+  username?: string,
+  user_id?: number
+): Promise<IResponse> {
+  let user = await prisma.users.findFirst({
+    where: { username },
+  });
+
+  if (!user && !moderatorLevel) return { status: 401, data: "User not found" };
+
+  user_id = moderatorLevel ? user_id : user?.UUID;
+
+  const userLoans = await prisma.user_loans.findMany({
+    where: { user_id },
+  });
+
   const itemsInLoan = await prisma.items_in_loan.findMany({
     where: { date_returned: null },
   });
@@ -10,7 +25,7 @@ export async function getAll(): Promise<IResponse> {
     if (userLoan.Produkt_status) continue;
 
     const itemInLoan = itemsInLoan.find(
-      ({ item_id }) => item_id === userLoan.UUID
+      ({ item_id }) => item_id === userLoan.item_id
     );
 
     userLoan.Produkt_status = itemInLoan ? "Lånt ud" : "Tilgængelig";
