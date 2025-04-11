ed29ac2a9a15016fa1d60dfec1e8f0fe2a5cdc8f
Nima Badiei <nimabadiei@gmail.com>
2025-04-11 11:27:32 +0200

    refactor: Remove minModLevel middleware from user loans route

--- backend/src/api/routes/user_loans.ts
+++ backend/src/api/routes/user_loans.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 
 import * as userLoansController from "@controllers/user_loans";
-import { minModLevel } from "@middleware/auth";
 
 const router = Router();
 
-router.get("/", minModLevel(1), userLoansController.GetAll());
+router.get("/", userLoansController.GetAll());
 
 export default router;
