e7cbcda1be7aa8de8a40f21a52725b2254bf2876
Nima Badiei <nimabadiei@gmail.com>
2025-04-22 08:28:56 +0200

    Merge pull request #114 from ItzNimaB/dev
    refactor: Update sendMail call to use MailOptions object for improved…

--- backend/src/api/routes/mail.ts
+++ backend/src/api/routes/mail.ts
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
 
   const { to, subject, text } = req.body;
 
-  const response = await sendMail(to, subject, text);
+  const response = await sendMail({ to, subject, text });
 
   res.send(response);
 });
